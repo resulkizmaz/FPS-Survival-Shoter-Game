c35e28271a01e04cb8e18be8db5c6d2380d18059
Resul KIZMAZ <algast12@gmail.com>
2022-06-17 01:11:57 +0300

    jump

--- FPS Survival-Shoter Game/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ FPS Survival-Shoter Game/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -19,23 +19,38 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        MoveThePlayer();
+        playerMove();
     }
-    void MoveThePlayer() //Karatkeri hareket ettirelim
+    void playerMove() //Karatkeri hareket ettirelim
     {
         moveDirection = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0, Input.GetAxis(Axis.VERTICAL)); // eksenlerde yazým yanlýþý yapmamak için.
         moveDirection = transform.TransformDirection(moveDirection);
         moveDirection *= speed * Time.deltaTime; // deltaTime ile çarpmamýzýn sebebi baðlý fonksiyonu update'de çaðýrdýðýmýz için smoot'luðu saðlamasýný istememiz.
 
-        ApplyGravity();
+        applyGravity();
+
         characterController.Move(moveDirection); // bu komut içerisinde Vector3 tutan unity takýmý tarafýndan oluþturulan bir metod.
+
     }// MoveThePlayer
-    void ApplyGravity() // Yerçekimini kontrol edip karakteri zýplatalým.
+
+    void applyGravity() // Yerçekimini kontrol edip karakteri zýplatalým.
     {
+        verticalVelocity -= gravity * Time.deltaTime; // Yerde olduðumuzda YERDE KALMAMIZI SAÐLAYACAK.
 
+        playerJump();
 
+        moveDirection.y = verticalVelocity * Time.deltaTime;
 
     }//ApplyGravity
+
+    void playerJump()
+    {
+        if (characterController.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            verticalVelocity = jumpForce;
+        }
+
+    } // PlayerJump
 
 
 }
